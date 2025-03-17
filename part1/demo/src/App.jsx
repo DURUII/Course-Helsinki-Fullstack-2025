2b04c72ba24c36fa5b584519dcee599497dea6df
DURUII <wust.durui@qq.com>
2025-03-17 16:17:45 +0800

    An event handler is a function

--- part1/demo/src/App.jsx
+++ part1/demo/src/App.jsx
@@ -1,27 +1,18 @@
 import { useState } from 'react'
 
 const App = () => {
-  // The convention is to name state variables like 
-  // [something, setSomething] using array destructuring.
-  // useState is a Hook, so you can only call it 
-  // at the top level of your component or your own Hooks. 
   const [counter, setCounter] = useState(0)
-
-  // The set function that lets you change 
-  // it to any other value in response to interaction.
-  setTimeout(
-    // arrow function again
-    // Calling the set function does not 
-    // change the current state 
-    // in the already executing code
-    () => setCounter(counter + 1),
-    1000
-  )
-
-  console.log('rendering...', counter)
-
+  // An event handler is a function
   return (
-    <div>{counter}</div>
+    <>
+      <div>{counter}</div>
+      <button onClick={() => { setCounter(counter + 1) }}>
+        plus
+      </button>
+      <button onClick={() => { setCounter(0) }}>
+        zero
+      </button>
+    </>
   )
 }
 
