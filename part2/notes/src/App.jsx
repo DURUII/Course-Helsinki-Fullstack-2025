f1eb1e590e0422475a88740085c4c32a216486ea
DURUII <wust.durui@qq.com>
2025-05-08 15:43:15 +0800

    docs: add comments for part2-1

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,14 +1,62 @@
 import Note from './components/Note'
+import { useState } from "react"
 
-const App = ({ notes }) => {
+const App = (props) => {
+  // React Developer Tools may be helpful
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note')
+  const [showAll, setShowAll] = useState(true)
+
+  const addNote = (event) => {
+    // prevent the default action of submitting a form
+    event.preventDefault()
+
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: String(notes.length + 1),
+    }
+
+    // a new copy of the array 
+    // with the new item added to the end
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+    // console.log('button clicked', event.target)
+  }
+
+  // input value + onChange event [controlled component]
+  // the uncontrolled style is to use a ref, and handle the onClick event
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
+  }
+
+  // conditional operator
+  const notesToShow = showAll ?
+    notes :
+    notes.filter(note => note.important)
+
   return (
     <div>
       <h1>Notes</h1>
+
+      <div>
+        <button onClick={() => setShowAll(!showAll)} >
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map(note =>
           <Note key={note.id} note={note} />
-        ))}
+        )}
       </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote}
+          onChange={handleNoteChange} />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 }
